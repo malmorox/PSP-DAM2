e897a233c73ff8ca896678c212f8c47a2245f7b7
malmorox <marcos.almorox@gmail.com>
2025-09-19 13:43:05 +0200

    añadiendo ejercicio de acceso a los hilos

---
+++ Inicialización/Procesos e hilos básicos/Contador.java
@@ -0,0 +1,42 @@
+package exercises;
+
+import java.util.ArrayList;
+
+public class Contador {
+    private int contador;
+    private ArrayList<Acceso> accesos;
+
+    public Contador() {
+        super();
+        this.contador = 0;
+        this.accesos = new ArrayList<>();
+    }
+
+    public synchronized void incrementar(String nombreHilo) {
+        this.contador++;
+
+        Acceso accesoExistente = null;
+        for (Acceso a : accesos) {
+            if (a.getNombreHilo().equals(nombreHilo)) {
+                accesoExistente = a;
+                break;
+            }
+        }
+
+        if (accesoExistente != null) {
+            accesoExistente.incrementoAcceso();
+        } else {
+            Acceso nuevo = new Acceso(nombreHilo);
+            nuevo.incrementoAcceso();
+            accesos.add(nuevo);
+        }
+    }
+
+    public int getValor() {
+        return this.contador;
+    }
+
+    public ArrayList<Acceso> obtenerAccesosTotales() {
+        return this.accesos;
+    }
+}
